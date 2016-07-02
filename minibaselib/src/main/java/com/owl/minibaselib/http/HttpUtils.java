66b4f3db91acd797b7aa3c56d20674f536e085dc
Qiushunming <qiu592198740@gmail.com>
2016-07-02 14:31:02 +0800

    create project

---
+++ minibaselib/src/main/java/com/owl/minibaselib/http/HttpUtils.java
@@ -0,0 +1,28 @@
+package com.owl.minibaselib.http;
+
+/**
+ * Created by qiushunming on 16/6/30.
+ */
+public class HttpUtils {
+
+    public interface OnResponse{
+
+    }
+
+    public static void post(){
+
+    }
+
+    public static void get(){
+
+    }
+
+    public static void upload(){
+
+    }
+
+    public static void download(){
+
+    }
+
+}
